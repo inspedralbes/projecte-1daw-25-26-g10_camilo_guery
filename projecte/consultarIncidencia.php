3511cd1974f3f986614d848273ff58613d13229e
a25campracar-ux <a25campracar@inspedralbes.cat>
2026-05-06 12:09:50 +0200

    Creació de l'historial de actuacions per l'usuari mostrant unicament actuacions marcades com visible pel tecnic asignat.

--- projecte/consultarIncidencia.php
+++ projecte/consultarIncidencia.php
@@ -3,11 +3,32 @@ include_once "header.php";
 $mysqli = include_once "conexio.php";
 $idIncidencia = $_POST["idIncidencia"];
 $idIncidencia = (int)$idIncidencia;
-$resultado = $mysqli->query("SELECT idIncidencia, descripcio, data, idDepartament, idTecnic, idTipus, dataFinalitzacio, prioritat FROM INCIDENCIA WHERE idIncidencia = $idIncidencia");
-$incidencias = $resultado->fetch_all(MYSQLI_ASSOC);
+$resultado = $mysqli->prepare("SELECT i.idIncidencia, i.descripcio, i.data, i.dataFinalitzacio, i.prioritat,
+    d.nom AS nomDepartament, 
+    t.nom AS nomTecnic, 
+    p.nom AS nomTipus  
+FROM INCIDENCIA i 
+JOIN DEPARTAMENT d ON i.idDepartament = d.idDepartament
+JOIN TECNIC t ON i.idTecnic = t.idTecnic
+JOIN TIPUS p ON i.idTipus = p.idTipus
+WHERE idIncidencia = ?");
+
+$resultado->bind_param("i", $idIncidencia);
+$resultado->execute();
+
+$resultadoQuery = $resultado->get_result();
+$incidencias = $resultadoQuery->fetch_all(MYSQLI_ASSOC);
 ?>
 
-<table>
+<?php
+$sentencia = $mysqli->prepare("SELECT descripcio, data, visible, idIncidencia FROM ACTUACIO WHERE idIncidencia = ? AND visible = 1");
+$sentencia->bind_param("i", $idIncidencia);
+$sentencia->execute();
+$result = $sentencia->get_result();
+$actuacions = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<table class="table">
             <thead>
                 <tr>
                     <th>Id d'Incidencia</th>
@@ -27,14 +48,42 @@ $incidencias = $resultado->fetch_all(MYSQLI_ASSOC);
                         <td><?php echo $incidencia["idIncidencia"] ?></td>
                         <td><?php echo $incidencia["descripcio"] ?></td>
                         <td><?php echo $incidencia["data"] ?></td>
-                        <td><?php echo $incidencia["idDepartament"] ?></td>
-                        <td><?php echo $incidencia["idTecnic"] ?></td>
-                        <td><?php echo $incidencia["idTecnic"] ?></td>
-                        <td><?php echo $incidencia["idTipus"] ?></td>
+                        <td><?php echo $incidencia["nomDepartament"] ?></td>
+                        <td><?php echo $incidencia["nomTecnic"] ?></td>
+                        <td><?php echo $incidencia["nomTipus"] ?></td>
                         <td><?php echo $incidencia["dataFinalitzacio"] ?></td>
                         <td><?php echo $incidencia["prioritat"] ?></td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
-        <a class="" href="index.php">Volver</a>
+
+
+<?php if (!empty($actuacions)) { ?>
+<div>
+    <h3>Historial d'Actuacions</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Descripció</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($actuacions as $actuacio) { ?>
+                <tr>
+                    <td><?php echo $actuacio["data"]; ?></td>
+                    <td><?php echo $actuacio["descripcio"]; ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+<?php } else { ?>
+    <p>No hi ha actuacions enregistrades.</p>
+<?php } ?>
+</div>
+
+<a class="" href="index.php">Volver</a>
+
+<?php include_once "footer.php"; ?>
