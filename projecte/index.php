87c13447af983514e6accef90f3b144fc2863bad
a25campracar-ux <a25campracar@inspedralbes.cat>
2026-05-06 08:56:09 +0200

    Detalls de actuació d'Incidències i algo de bootstrap al llistat de tècnics

--- projecte/index.php
+++ projecte/index.php
@@ -5,7 +5,7 @@ include_once "header.php";
     <div class="text-center m-5">
         <h1>Identifica't</h1>
     </div>
-    <main>
+
         <div class="d-flex justify-content-center align-items-center">
             <div class="d-grid gap-3" style="width: 300px;">
                 <a href="persona.php" class="btn btn-info">Profesor</a>
@@ -13,7 +13,6 @@ include_once "header.php";
                 <a href="modificarIncidencies.php" class="btn btn-info">Responsable informàtic</a>
             </div>
         </div>
-    </main>
 
 </body>
 </html>
--- projecte/tecnic.php
+++ projecte/tecnic.php
@@ -5,13 +5,17 @@ $mysqli = include_once "conexio.php";
 $resultadoTecnicos = $mysqli->query("SELECT idTecnic, nom FROM TECNIC");
 $tecnicos = $resultadoTecnicos->fetch_all(MYSQLI_ASSOC);
 ?>
-    <main>
-        <div>
-            <ul>
-                <?php foreach ($tecnicos as $tecnico) { ?>
-                   <a href="llistatIncidenciaTecnic.php?idTecnic=<?php echo $tecnico["idTecnic"] ?>"> <li> <?php echo $tecnico["nom"]; ?> </li> </a>
-                <?php } ?>
-            </ul>
+    <div class="text-center m-5">
+    <h1>Qui ets?</h1>
+    </div>
+
+
+    <div class="justify-content-center d-flex">
+        <div class="d-grid gap-3" style="width: 300px;">
+            <?php foreach ($tecnicos as $tecnico) { ?>
+               <a href="llistatIncidenciaTecnic.php?idTecnic=<?php echo $tecnico["idTecnic"] ?>" class="btn btn-warning">  <?php echo $tecnico["nom"]; ?> </a>
+            <?php } ?>
         </div>
-    </main>    
+    </div>
+
 <?php include_once "footer.php"; ?>
