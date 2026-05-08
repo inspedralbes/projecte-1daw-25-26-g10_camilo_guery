b2fa4962719103cca8d4fff0948bc13838b6cec5
a25campracar-ux <a25campracar@inspedralbes.cat>
2026-05-08 10:52:19 +0200

    Bootstrap i mostrar departaments

--- projecte/persona.php
+++ projecte/persona.php
@@ -1,30 +1,43 @@
 <?php 
 $titulo = "Gestor d'Incidències - Persona";
 include_once "header.php"; 
+$mysqli = include_once "conexio.php";
+$resultadoDepartaments = $mysqli->query("SELECT idDepartament, nom FROM DEPARTAMENT");
+$departaments = $resultadoDepartaments->fetch_all(MYSQLI_ASSOC);
 ?>
-
-        <div>
-            <div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10 mt-5">
+            <div class="form-floating mb-3">
                 <h3>Registrar Incidència</h3>
                 <form action="registrar.php" method="POST">
-                    <label for="idDepartament">Departament:</label>
-                    <input type="text" name="idDepartament" id="idDepartament" placeholder="Introdueix l'ID del departament" required>
+                    <select class="form-select" for="idDepartament" name="idDepartament" id="idDepartament" required>
+                    <option selected>Departament:</option>
+                    <?php foreach ($departaments as $departament) { ?>
+                        <option value="<?php echo $departament["idDepartament"] ?>"><?php echo $departament["nom"]; ?></option>
+                    <?php } ?>
+                    </select>
                     <br>
                     <label for="descripcio">Descripcio</label>
-                    <textarea name="descripcio" id="descripcio" placeholder="Incidència: ">
+                    <textarea class="form-control" name="descripcio" id="descripcio" placeholder="Incidència: "  rows="3">
                     </textarea>
                     <br>
-                    <input type="submit" value="Enviar">
+                    <input class="btn btn-success" type="submit" value="Enviar">
                 </form>
             </div>
             <div>
                 <h3>Consultar Incidència</h3>
                 <form action="consultarIncidencia.php" method="POST">
-                    <label for="idIncidencia">ID de l'incidència</label>
-                    <input type="number" name="idIncidencia" id="idIncidencia" placeholder="ID d'Incidencia: ">
-                    <input type="submit" value="Enviar">
+                    <div class="input-group mt-2">
+                        <span class="input-group-text">ID</span>
+                        <input type="text" class="form-control" name="idIncidencia" id="idIncidencia" placeholder="ID d'Incidencia: ">
+                    </div>
+                    <br>
+                    <button type="submit" value="Enviar" class="btn btn-primary">Buscar</button>
+                    </div>
                 </form>
             </div>
         </div>
-        <a href="index.php">Tornar enrere</a>
+    </div>
+</div>
 <?php include_once "footer.php"; ?>
